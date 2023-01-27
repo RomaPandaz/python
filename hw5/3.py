ee7ac5f97efdb9605c2c5d29229bed64a1e214fe
student_1 <student@qwerty.com>
2023-01-27 18:15:10 +0300

    крестики нолики

--- hw5/3.py
+++ hw5/3.py
@@ -1,6 +1,6 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
-my_string = 'AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE11'  #сжатие
+my_string = 'AAAAAABDDCCCCCCCDDDEEE'  #сжатие
 def rle_in(my_string: str):
     final_string = ''
     count = 1
